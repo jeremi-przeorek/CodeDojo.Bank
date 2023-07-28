73c63c1de42c0a7cf900c513f2bfabb92dfaacde
jeremiprzeorek <jeremi.przeorek@gmail.com>
2023-07-28 14:00:31 +0200

    Part 3 Completed

--- CodeDojo.Bank/Program.cs
+++ CodeDojo.Bank/Program.cs
@@ -16,6 +16,20 @@
             { new[] { " _ ", "|_|", " _|" }, '9' }
         };
 
+        private static readonly IReadOnlyDictionary<char, char[]> _similarDigits = new Dictionary<char, char[]>()
+        {
+            { '0', new[] { '8' } },
+            { '1', new[] { '7' } },
+            { '2', Array.Empty<char>() },
+            { '3', new[] { '9' } },
+            { '4', Array.Empty<char>() },
+            { '5', new[] { '9', '6' } },
+            { '6', new[] { '5', '8' } },
+            { '7', new[] { '1' } },
+            { '8', new[] { '0', '6', '9' } },
+            { '9', new[] { '8', '5', '3' } }
+        };
+
         private const char InvalidChar = '?';
 
         static void Main(string[] args)
@@ -26,7 +40,7 @@
 
             for (int i = 0; i < text.Length - 4; i += 4)
             {
-                var linesToProcess = text[i..(i + 4)]; //WOOOOOW
+                var linesToProcess = text[i..(i + 4)];
                 for (int j = 0; j < linesToProcess[0].Length / 3; j++)
                 {
                     var top = new string(linesToProcess[0].Skip(j * 3).Take(3).ToArray());
@@ -55,9 +69,43 @@
                     string e when !IsCheckSumValid(e) => entry + " ERR",
                     _ => entry
                 };
+                
                 Console.WriteLine(entry);
 
-                
+                if (entry.EndsWith("ERR"))
+                {
+                    List<string> validAlternatives = new();
+                    string digits = entry[0..9];
+                    for (var index = 0; index < digits.Length; index++)
+                    {
+                        var digit = digits[index];
+                        char[] possibleDigits = _similarDigits[digit];
+
+                        foreach (var possibleDigit in possibleDigits)
+                        {
+                            char[] digitsCopy = digits.ToCharArray();
+                            digitsCopy[index] = possibleDigit;
+
+                            string input = new string(digitsCopy);
+                            if (IsCheckSumValid(input))
+                            {
+                                validAlternatives.Add(input);
+                            }
+                        }
+                    }
+
+                    if (validAlternatives.Count == 1)
+                    {
+                        Console.WriteLine(validAlternatives[0]);
+                    }
+
+                    if (validAlternatives.Count() > 1)
+                    {
+                        validAlternatives.ForEach(x => Console.WriteLine(x));
+                    }
+                }
+
+
                 output.Clear();
             }
         }
@@ -83,7 +131,7 @@
             return sum % 11 == 0;
         }
 
-        static bool IsIllegible(string input) 
+        static bool IsIllegible(string input)
             => input.Any(x => x is '?');
     }
 }
